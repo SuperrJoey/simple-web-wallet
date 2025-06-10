45d3d82b08e25478cebe844b97baa04005c4dd96
SuperrJoey <devdaryaan@gmail.com>
2025-06-10 23:08:50 +0530

    added keypair generation

--- src/SolanaWallet.jsx
+++ src/SolanaWallet.jsx
@@ -2,10 +2,33 @@ import { useState } from "react";
 import { derivePath } from "ed25519-hd-key";
 import { Keypair } from "@solana/web3.js";
 import nacl from "tweetnacl";
+import { mnemonicToSeed } from "bip39";
 
 export function SolanaWallet({ mnemonic }) {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [publicKeys, setPublicKeys] = useState([]);
+
+    //actual derivation funcion
+    const addWallet = async () => {
+        // Step 1: Converting mnemonic to seed (512-bit)
+        const seed = await mnemonicToSeed(mnemonic);
+
+        // Step 2: Create derivation path
+        const path = `m/44'/501'/${currentIndex}'/0'`; //building the path
+
+        //Step 3: Derivep private key from seed + path
+        const derivedSeed = derivePath(path, seed.toString("hex")).key;
+
+        //Step 4: Generate Solana Keypair from derived seed
+        const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+        const keypair = Keypair.fromSecretKey(secret);
+
+        //Step 5 : get public address
+        const publicKey = keypair.publicKey.toBase58();
+
+        setCurrentIndex(currentIndex + 1);
+        setPublicKeys([...publicKeys, publicKey]);
+    }
 
     return (
         <div className="bg-purple-50 border border-purple-200 rounded-lg p-6 mt-4">
@@ -16,7 +39,7 @@ export function SolanaWallet({ mnemonic }) {
             <button
               className="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded font-medium mb-4"
                 onClick={() => {
-                    console.log("Adding Solana wallet...");
+                    addWallet();
                 }}
                 >
                 Add Wallet
